9caefd913e016ae6234e09178a8796f88ae5d874
tenebricosa <elliot.wrt@gmail.com>
2018-05-15 19:03:09 +0500

    add start screen

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app">
+                <header className="app__header">
+                    <h1 className="app__logo">Frontend<br />Logo<br />Quiz</h1>
+                </header>
+                <div className="app__form">
+                    <input type="text" placeholder="Имя Фамилия" className="app__input"/>
+                    <input type="text" placeholder="Email" className="app__input" />
+                    <button className="app__start-button">Поехали!</button>
+                </div>
+                <footer className="app__footer">
+                    <button className="app__start-button--test">Играть за Славяна</button>
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default App;
