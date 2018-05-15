34627a21f317503da89b63458c749ac9890659fd
tenebricosa <elliot.wrt@gmail.com>
2018-05-15 10:53:51 +0500

    add firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyAa0I6z7tLDMMtYx9SnR3PNINXT0czmeh0",
+    authDomain: "frontend-logo-quiz.firebaseapp.com",
+    databaseURL: "https://frontend-logo-quiz.firebaseio.com",
+    projectId: "frontend-logo-quiz",
+    storageBucket: "",
+    messagingSenderId: "349628808944"
+}
