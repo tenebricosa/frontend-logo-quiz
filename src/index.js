e735d901a4ba5adfe5629b96944b277dcb2578ec
tenebricosa <elliot.wrt@gmail.com>
2018-05-16 11:21:44 +0500

    init store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, compose } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducer';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const isDev = process.env.NODE_ENV !== 'production';
+
+const includeDevTools = () => {
+    if (isDev && window && window.devToolsExtension) {
+        return window.devToolsExtension();
+    }
+
+    return f => f;
+};
+
+const initialState = {};
+
+const store = createStore(
+    rootReducer,
+    initialState,
+    compose(includeDevTools())
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
